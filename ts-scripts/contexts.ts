2aba45000ef02593ab0ee0858bdca13124b6fecb
Achthar <69119535+Achthar@users.noreply.github.com>
2024-11-05 15:11:31 +0100

    add read to mainnet

--- ts-scripts/contexts.ts
+++ ts-scripts/contexts.ts
@@ -1,9 +1,4 @@
 export namespace TestnetData {
-    export const TOKENS = {
-        USDT: '0x3f007b72f7bcb9b1e9abe2c76e63790cd574b7c34f1c91d6c2f407a5b55676b9',
-        BTC: '0xce90621a26908325c42e95acbbb358ca671a9a7b36dfb6a5405b407ad1efcd30',
-        ETH: '0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07'
-    }
 
     export const MIRA_ADD_LIQUIDITY_SCRIPT = "0xf81922cb5fdc213d65b4843140f635cd4d034ea384626c834cbcfec9e10ebd05"
 
@@ -41,6 +36,69 @@ export namespace TestnetData {
         name: 'Test_ETH',
         symbol: 'T_ETH',
         assetId: '0xe059c6380d9cd768957ef9081d229e64ae641256f5ec4d98191b8b5cc9b91b72'
+    }
+
+}
+
+export namespace MainnetData {
+
+    export const TOKEN_GATEWAY = "0x4ea6ccef1215d9479f1024dff70fc055ca538215d2c8c348beddffd54583d0e8"
+
+    export const RPC = "https://mainnet.fuel.network/v1/graphql"
+
+    export const USDT = {
+        "chainId": 9889,
+        "name": "USDT",
+        "address": "0xa0265fb5c32f6e8db3197af3c7eb05c48ae373605b8165b6f4a51c5b0ba4812e",
+        "symbol": "USDT",
+        "decimals": 6,
+        "logoURI": "https://verified-assets.fuel.network/images/usdt.svg",
+        "contractId": "0x4ea6ccef1215d9479f1024dff70fc055ca538215d2c8c348beddffd54583d0e8",
+        "subId": "0x033dd0c9246b67aea1456ed2ea6aaac61253583b7a1c84cd73d968ad7d30b72d"
+    }
+
+    export const USDC = {
+        "chainId": 9889,
+        "name": "USDC",
+        "address": "0x286c479da40dc953bddc3bb4c453b608bba2e0ac483b077bd475174115395e6b",
+        "symbol": "USDC",
+        "decimals": 6,
+        "logoURI": "https://verified-assets.fuel.network/images/usdc.svg",
+        "contractId": "0x4ea6ccef1215d9479f1024dff70fc055ca538215d2c8c348beddffd54583d0e8",
+        "subId": "0xb2114fe7ae91bdf9c23145ee523b2f59fdd7a3c542b93de75c9551873d429757"
+    }
+
+    export const FBTC = {
+        "chainId": 9889,
+        "name": "FBTC",
+        "address": "0xb5ecb0a1e08e2abbabf624ffea089df933376855f468ade35c6375b00c33996a",
+        "symbol": "FBTC",
+        "decimals": 8,
+        "logoURI": "https://verified-assets.fuel.network/images/fbtc.svg",
+        "contractId": "0x4ea6ccef1215d9479f1024dff70fc055ca538215d2c8c348beddffd54583d0e8",
+        "subId": "0x5ecf00904c88f891048f2affb621a87c6392375fdb3c051dd7129526b4b0da55"
+    }
+
+    export const WETH = {
+        "chainId": 9889,
+        "name": "WETH",
+        "address": "0xa38a5a8beeb08d95744bc7f58528073f4052b254def59eba20c99c202b5acaa3",
+        "symbol": "WETH",
+        "decimals": 9,
+        "logoURI": "https://verified-assets.fuel.network/images/weth.svg",
+        "contractId": "0x4ea6ccef1215d9479f1024dff70fc055ca538215d2c8c348beddffd54583d0e8",
+        "subId": "0xd4d7948d31e97d4ed6d0182814e8bc827dfe0d8e70a010bdb9e34aae92775491"
+    }
+
+    export const ETH = {
+        "chainId": 9889,
+        "name": "Ethereum",
+        "address": "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07",
+        "symbol": "ETH",
+        "decimals": 9,
+        "logoURI": "https://verified-assets.fuel.network/images/eth.svg",
+        "contractId": "",
+        "subId": ""
     }
 
 }
--- ts-scripts/token/readToken.ts
+++ ts-scripts/token/readToken.ts
@@ -1,26 +1,23 @@
 import { Provider, Wallet } from "fuels";
 import { MNEMONIC } from "../env";
-import { TestnetData } from "../contexts";
+import { MainnetData } from "../contexts";
 import { MockToken } from "../typegen/MockToken";
 import { assetIdInput } from "../utils";
 
-const assetId: string = TestnetData.USDT.assetId
+const assetId: string = MainnetData.USDT.address
 
 async function main() {
-    const provider = await Provider.create(TestnetData.RPC);
+    const provider = await Provider.create(MainnetData.RPC);
     const wallet = Wallet.fromMnemonic(MNEMONIC!, undefined, undefined, provider);
 
-    const MockTokenContract = new MockToken(TestnetData.MOCK_TOKEN, wallet)
-
-    const tx = await MockTokenContract.functions.symbol(assetIdInput(assetId)).simulate()
-    console.log("tx", tx.value)
+    const MockTokenContract = new MockToken(MainnetData.TOKEN_GATEWAY, wallet)
 
     const tx2 = await MockTokenContract.multiCall(
         [
             MockTokenContract.functions.symbol(assetIdInput(assetId)),
             MockTokenContract.functions.decimals(assetIdInput(assetId)),
             MockTokenContract.functions.name(assetIdInput(assetId)),
-        ]).simulate()
+        ]).dryRun()
     console.log("tx2", tx2.value)
 }
 

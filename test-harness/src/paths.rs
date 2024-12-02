6aead153f4629c0a92375aaccf1a2cbcc2d070b8
Achthar <69119535+Achthar@users.noreply.github.com>
2024-12-02 17:16:20 +0100

    fix configs

--- test-harness/src/paths.rs
+++ test-harness/src/paths.rs
@@ -8,4 +8,4 @@ pub const BATCH_SWAP_EXACT_OUTPUT_SCRIPT_BINARY_PATH: &str =
     "../../scripts/batch_swap_exact_out_script/out/debug/batch_swap_exact_out_script.bin";
     
 pub const AMM_CONTRACT_BINARY_PATH: &str = "../../fixtures/mira-amm/mira_amm_contract.bin";
-pub const MOCK_TOKEN_CONTRACT_BINARY_PATH: &str = "../../fixtures/mock-token/mock_token.bin";
+pub const MOCK_TOKEN_CONTRACT_BINARY_PATH: &str = "../../contracts/mocks/mock_token/out/debug/mock_token.bin";

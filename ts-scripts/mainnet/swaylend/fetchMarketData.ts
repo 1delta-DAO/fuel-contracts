189c760a8fc2f86cf99d8d2d864a8f1bcd84e179
caglavol <volkan@1delta.io>
2025-04-04 17:42:58 +0200

    fetch additional market data

--- ts-scripts/mainnet/swaylend/fetchMarketData.ts
+++ ts-scripts/mainnet/swaylend/fetchMarketData.ts
@@ -1,6 +1,7 @@
-import { Contract, Provider, Wallet } from "fuels";
+import { AssetId, Contract, Provider, Wallet } from "fuels";
 import { MainnetData } from "../../contexts";
 import SWAYLEND_ABI from "../../../fixtures/swaylend/market-abi.json";
+import { IdentityInput } from "../../typegen/BatchSwapExactInScript";
 
 async function main() {
     const provider = new Provider(MainnetData.RPC);
@@ -9,10 +10,27 @@ async function main() {
 
     const { value: collateralConfigurations } = await swaylend.functions.get_collateral_configurations().get()
     const { value: marketConfiguration } = await swaylend.functions.get_market_configuration().get()
-    
+    const { value: marketConfigurationWithInterest } = await swaylend.functions.get_market_basics_with_interest().get()
+    const { value: reserves } = await swaylend.functions.get_reserves().get()
+
+    const assetId: AssetId = { bits: MainnetData.ETH.address }
+    const { value: collateralReserves } = await swaylend.functions.get_collateral_reserves(assetId).get()
+    const { value: balanceOf } = await swaylend.functions.balance_of(assetId).get()
+
+    const identity: IdentityInput = { Address: { bits: "0x80Ea2b1812bE28BF7Cf8cDf95438A964DE549DF9EfEC24509482A5C7Be55DE7B" } }
+    const { value: userBasic } = await swaylend.functions.get_user_basic(identity).get()
+    const { value: userBasicWithInterest } = await swaylend.functions.get_user_balance_with_interest(identity).get()
+
+
     console.log("fetched data:")
     console.log("collateralConfigurations:", collateralConfigurations)
     console.log("marketConfiguration:", marketConfiguration)
+    console.log("marketConfigurationWithInterest:", marketConfigurationWithInterest)
+    console.log("reserves:", reserves)
+    console.log("collateralReserves:", collateralReserves)
+    console.log("balanceOf ETH:", balanceOf)
+    console.log("userBasic:", userBasic)
+    console.log("userBasicWithInterest:", userBasicWithInterest)
 }
 
 main()

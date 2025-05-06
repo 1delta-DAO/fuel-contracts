7a1334f6bb4effc77a03d645165d0e1bae3e9d58
Achthar <69119535+Achthar@users.noreply.github.com>
2025-05-06 14:33:44 +0200

    setup first test

--- test/accountFactory.test.ts
+++ test/accountFactory.test.ts
@@ -1,92 +1,58 @@
 import { launchTestNode } from 'fuels/test-utils';
 import { describe, test, expect } from 'vitest';
-import { addressInput } from '../ts-scripts/utils';
+import { addressInput, contractIdInput } from '../ts-scripts/utils';
 import { OrderInput } from '../ts-scripts/typegen/OneDeltaOrders';
 import { OrderTestUtils } from './utils';
 import { ZeroBytes32 } from 'fuels';
+import { AccountFactoryFactory } from '../ts-scripts/typegen';
+import { AccountProxyFactory } from '../ts-scripts/typegen/AccountProxyFactory';
 
-describe('Order fill via `msg_amount`', async () => {
+describe('factory creations', async () => {
 
-  test('If attempt to fill more than taker_amount, do not error', async () => {
+  test('different roots for same contract with different configurables', async () => {
     const launched = await launchTestNode({ walletsConfig: { count: 3 } });
 
     const {
       wallets: [maker, deployer, taker]
     } = launched;
 
-    
-    const { Orders, tokens } = await OrderTestUtils.fixture(deployer)
 
-    const [maker_asset, taker_asset] = await OrderTestUtils.createTokens(deployer, OrderTestUtils.contractIdBits(tokens))
-
-    await OrderTestUtils.fundWallets(
-      [maker, taker],
-      OrderTestUtils.contractIdBits(tokens),
-      [maker_asset, taker_asset],
-      [OrderTestUtils.DEFAULT_MINT_AMOUNT, OrderTestUtils.DEFAULT_MINT_AMOUNT]
-    )
-
-    const maker_amount = OrderTestUtils.getRandomAmount()
-
-    await OrderTestUtils.getOrders(maker, OrderTestUtils.contractIdBits(Orders)).functions.deposit(maker_asset, addressInput(maker.address))
-      .callParams({ forward: { assetId: maker_asset, amount: maker_amount } })
-      .call()
-
-
-    const taker_amount = OrderTestUtils.getRandomAmount()
-
-    const order: OrderInput = OrderTestUtils.getOrder({
-      maker_asset,
-      taker_asset,
-      maker_amount,
-      taker_amount,
-      maker: maker.address.toB256(),
-      nonce: OrderTestUtils.getRandomAmount(1),
-      maker_traits: OrderTestUtils.MAX_EXPIRY,
-      maker_receiver: ZeroBytes32
+    const factoryTx = await AccountFactoryFactory.deploy(deployer, {
+      configurableConstants: {
+        TEMPLATE_BYTECODE_ROOT: deployer.address.b256Address,
+        BEACON_ADDRESS: deployer.address.b256Address,
+      }
     })
+    const { contract: factory } = await factoryTx.waitForResult()
 
-    const taker_fill_amount = taker_amount.add(1)
+    let templatAccountTx = await AccountProxyFactory.deploy(deployer, {
+      configurableConstants: {
+        BEACON: deployer.address.b256Address
+      }
+    })
+    const { contract: account0 } = await templatAccountTx.waitForResult()
 
-    const signatureRaw = await maker.signMessage(OrderTestUtils.packOrder(order, Orders))
+    // @ts-ignore
+    let result = await factory.functions.bytecode_root(contractIdInput(account0.id).ContractId).simulate()
 
 
-    const [
-      taker_maker_asset_balance_before,
-      taker_taker_asset_balance_before
-    ] = await OrderTestUtils.getConventionalBalances(
-      taker,
-      [maker_asset, taker_asset]
-    )
+    const firstRoot = result.value
+    console.log("firstRoot", firstRoot)
 
-    await OrderTestUtils.getOrders(taker, OrderTestUtils.contractIdBits(Orders)).functions.fill(
-      order,
-      signatureRaw,
-      taker_fill_amount,
-      addressInput(taker.address)
-    )
-      .callParams({ forward: { assetId: taker_asset, amount: taker_fill_amount } })
-      .call()
+    templatAccountTx = await AccountProxyFactory.deploy(deployer, {
+      configurableConstants: {
+        BEACON: maker.address.b256Address
+      }
+    })
+    const { contract: account1 } = await templatAccountTx.waitForResult()
 
-    const [
-      taker_maker_asset_balance_after,
-      taker_taker_asset_balance_after
-    ] = await OrderTestUtils.getConventionalBalances(
-      taker,
-      [maker_asset, taker_asset]
-    )
+    // @ts-ignore
+    result = await factory.functions.bytecode_root(contractIdInput(account1.id).ContractId).simulate()
 
-    // validate taker change -> it settled for taker_amount
-    expect(
-      taker_maker_asset_balance_after.sub(taker_maker_asset_balance_before).toString()
-    ).to.equal(
-      maker_amount.toString()
-    )
-    expect(
-      taker_taker_asset_balance_before.sub(taker_taker_asset_balance_after).toString()
-    ).to.equal(
-      taker_amount.add(1).toString()
-    )
 
+    const secondRoot = result.value
+    console.log("secondRoot", secondRoot)
+
+    expect(secondRoot).to.not.equal(firstRoot)
   });
 });
